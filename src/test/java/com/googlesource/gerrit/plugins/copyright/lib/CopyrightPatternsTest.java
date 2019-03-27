78d26501a32882984fd0bdcaa064385be806f729
Bob Badour <bbadour@google.com>
2019-03-26 20:07:36 -0700

    Release copyright scanner library
    This copyright scanner library has been developed specifically for
    creating a plugin to ensure necessary review when new revisions may
    introduce non-compliant copyrights.
    
    Initially targeted at the AOSP gerrit instances with the intent to
    expand to other Google-owned instances, the plugin will be completely
    configurable.
    
    Best practices identify allowed copyrights as first party or third
    party, and restrict third party copyrights to specific locations.
    
    See for example: https://opensource.google.com/docs/thirdparty/
    
    Revisions licensed entirely under first party licenses do not require
    special review.
    
    Revisions to files in locations where third party licenses are allowed
    that are licensed entirely under first party or third party licenses
    likewise do not require special review.
    
    Revisions to files outside locations where third party licenses are
    allowed that appear to have third party licenses will require special
    review to verify they are false positives, or a qualified reviewer may
    reject the commit.
    
    Changes to files that seem to introduce unknown or forbidden licenses
    likewise require special review. A qualified reviewer may determine
    that an unknown license has acceptable terms and allow it. The reviewer
    may determine the match is a false positive or reject the change.
    
    See for example: https://opensource.google.com/docs/thirdparty/licenses/
    and: https://opensource.google.com/docs/using/agpl-policy/
    
    This commit does not include the plugin. It releases the copyright
    scanner library externally. The library has been extensively tested and
    used internally for analyzing content on AOSP hosts.
    
    The library and associated command-line tool supports deep scans into
    archive files (.zip, .jar, .apk etc.); however, the plugin will perform
    only shallow scans.
    
    Change-Id: I8800cf011f392d7d0c848f43a8efa095dd68ad0a

---
+++ src/test/java/com/googlesource/gerrit/plugins/copyright/lib/CopyrightPatternsTest.java
@@ -0,0 +1,240 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.copyright.lib;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CopyrightPatternsTest {
+
+  @Test
+  public void testBuildEmpty() {
+    CopyrightPatterns.RuleSet.Builder builder = CopyrightPatterns.RuleSet.builder();
+
+    CopyrightPatterns.RuleSet rules = builder.build();
+
+    assertThat(rules.excludePatterns).isEmpty();
+    assertThat(rules.firstPartyLicenses).isEmpty();
+    assertThat(rules.firstPartyOwners).isEmpty();
+    assertThat(rules.thirdPartyLicenses).isEmpty();
+    assertThat(rules.thirdPartyOwners).isEmpty();
+    assertThat(rules.forbiddenLicenses).isEmpty();
+    assertThat(rules.forbiddenOwners).isEmpty();
+  }
+
+  @Test
+  public void testBuildExcludePattern() {
+    CopyrightPatterns.RuleSet.Builder builder =
+        CopyrightPatterns.RuleSet.builder().excludePattern("pattern");
+
+    CopyrightPatterns.RuleSet rules = builder.build();
+
+    assertThat(rules.excludePatterns).containsExactly("pattern");
+    assertThat(rules.firstPartyLicenses).isEmpty();
+    assertThat(rules.firstPartyOwners).isEmpty();
+    assertThat(rules.thirdPartyLicenses).isEmpty();
+    assertThat(rules.thirdPartyOwners).isEmpty();
+    assertThat(rules.forbiddenLicenses).isEmpty();
+    assertThat(rules.forbiddenOwners).isEmpty();
+  }
+
+  @Test
+  public void testBuildFirstPartyLicense() {
+    CopyrightPatterns.RuleSet.Builder builder =
+        CopyrightPatterns.RuleSet.builder().addFirstPartyLicense("pattern");
+
+    CopyrightPatterns.RuleSet rules = builder.build();
+
+    assertThat(rules.excludePatterns).isEmpty();
+    assertThat(rules.firstPartyLicenses).containsExactly("pattern");
+    assertThat(rules.firstPartyOwners).isEmpty();
+    assertThat(rules.thirdPartyLicenses).isEmpty();
+    assertThat(rules.thirdPartyOwners).isEmpty();
+    assertThat(rules.forbiddenLicenses).isEmpty();
+    assertThat(rules.forbiddenOwners).isEmpty();
+  }
+
+  @Test
+  public void testBuildFirstPartyOwner() {
+    CopyrightPatterns.RuleSet.Builder builder =
+        CopyrightPatterns.RuleSet.builder().addFirstPartyOwner("pattern");
+
+    CopyrightPatterns.RuleSet rules = builder.build();
+
+    assertThat(rules.excludePatterns).isEmpty();
+    assertThat(rules.firstPartyLicenses).isEmpty();
+    assertThat(rules.firstPartyOwners).containsExactly("pattern");
+    assertThat(rules.thirdPartyLicenses).isEmpty();
+    assertThat(rules.thirdPartyOwners).isEmpty();
+    assertThat(rules.forbiddenLicenses).isEmpty();
+    assertThat(rules.forbiddenOwners).isEmpty();
+  }
+
+  @Test
+  public void testBuildThirdPartyLicense() {
+    CopyrightPatterns.RuleSet.Builder builder =
+        CopyrightPatterns.RuleSet.builder().addThirdPartyLicense("pattern");
+
+    CopyrightPatterns.RuleSet rules = builder.build();
+
+    assertThat(rules.excludePatterns).isEmpty();
+    assertThat(rules.firstPartyLicenses).isEmpty();
+    assertThat(rules.firstPartyOwners).isEmpty();
+    assertThat(rules.thirdPartyLicenses).containsExactly("pattern");
+    assertThat(rules.thirdPartyOwners).isEmpty();
+    assertThat(rules.forbiddenLicenses).isEmpty();
+    assertThat(rules.forbiddenOwners).isEmpty();
+  }
+
+  @Test
+  public void testBuildThirdPartyOwner() {
+    CopyrightPatterns.RuleSet.Builder builder =
+        CopyrightPatterns.RuleSet.builder().addThirdPartyOwner("pattern");
+
+    CopyrightPatterns.RuleSet rules = builder.build();
+
+    assertThat(rules.excludePatterns).isEmpty();
+    assertThat(rules.firstPartyLicenses).isEmpty();
+    assertThat(rules.firstPartyOwners).isEmpty();
+    assertThat(rules.thirdPartyLicenses).isEmpty();
+    assertThat(rules.thirdPartyOwners).containsExactly("pattern");
+    assertThat(rules.forbiddenLicenses).isEmpty();
+    assertThat(rules.forbiddenOwners).isEmpty();
+  }
+
+  @Test
+  public void testBuildForbiddenLicense() {
+    CopyrightPatterns.RuleSet.Builder builder =
+        CopyrightPatterns.RuleSet.builder().addForbiddenLicense("pattern");
+
+    CopyrightPatterns.RuleSet rules = builder.build();
+
+    assertThat(rules.excludePatterns).isEmpty();
+    assertThat(rules.firstPartyLicenses).isEmpty();
+    assertThat(rules.firstPartyOwners).isEmpty();
+    assertThat(rules.thirdPartyLicenses).isEmpty();
+    assertThat(rules.thirdPartyOwners).isEmpty();
+    assertThat(rules.forbiddenLicenses).containsExactly("pattern");
+    assertThat(rules.forbiddenOwners).isEmpty();
+  }
+
+  @Test
+  public void testBuildForbiddenOwner() {
+    CopyrightPatterns.RuleSet.Builder builder =
+        CopyrightPatterns.RuleSet.builder().addForbiddenOwner("pattern");
+
+    CopyrightPatterns.RuleSet rules = builder.build();
+
+    assertThat(rules.excludePatterns).isEmpty();
+    assertThat(rules.firstPartyLicenses).isEmpty();
+    assertThat(rules.firstPartyOwners).isEmpty();
+    assertThat(rules.thirdPartyLicenses).isEmpty();
+    assertThat(rules.thirdPartyOwners).isEmpty();
+    assertThat(rules.forbiddenLicenses).isEmpty();
+    assertThat(rules.forbiddenOwners).containsExactly("pattern");
+  }
+
+  @Test
+  public void testBuildEachNamedRuleExclusion() {
+    for (String ruleName : CopyrightPatterns.lookup.keySet()) {
+      CopyrightPatterns.RuleSet.Builder builder =
+          CopyrightPatterns.RuleSet.builder().exclude(ruleName);
+
+      CopyrightPatterns.RuleSet rules = builder.build();
+
+      CopyrightPatterns.Rule rule = CopyrightPatterns.lookup.get(ruleName);
+
+      if (rule.exclusions != null) {
+        assertThat(rules.excludePatterns).containsAllIn(rule.exclusions);
+      }
+      if (rule.licenses != null) {
+        assertThat(rules.excludePatterns).containsAllIn(rule.licenses);
+      }
+      if (rule.owners != null) {
+        assertThat(rules.excludePatterns).containsAllIn(rule.owners);
+      }
+    }
+  }
+
+  @Test
+  public void testBuildEachNamedRuleFirstParty() {
+    for (String ruleName : CopyrightPatterns.lookup.keySet()) {
+      CopyrightPatterns.RuleSet.Builder builder =
+          CopyrightPatterns.RuleSet.builder().addFirstParty(ruleName);
+
+      CopyrightPatterns.RuleSet rules = builder.build();
+
+      CopyrightPatterns.Rule rule = CopyrightPatterns.lookup.get(ruleName);
+
+      if (rule.exclusions != null) {
+        assertThat(rules.excludePatterns).containsExactlyElementsIn(rule.exclusions);
+      }
+      if (rule.licenses != null) {
+        assertThat(rules.firstPartyLicenses).containsExactlyElementsIn(rule.licenses);
+      }
+      if (rule.owners != null) {
+        assertThat(rules.firstPartyOwners).containsExactlyElementsIn(rule.owners);
+      }
+    }
+  }
+
+  @Test
+  public void testBuildEachNamedRuleThirdParty() {
+    for (String ruleName : CopyrightPatterns.lookup.keySet()) {
+      CopyrightPatterns.RuleSet.Builder builder =
+          CopyrightPatterns.RuleSet.builder().addThirdParty(ruleName);
+
+      CopyrightPatterns.RuleSet rules = builder.build();
+
+      CopyrightPatterns.Rule rule = CopyrightPatterns.lookup.get(ruleName);
+
+      if (rule.exclusions != null) {
+        assertThat(rules.excludePatterns).containsExactlyElementsIn(rule.exclusions);
+      }
+      if (rule.licenses != null) {
+        assertThat(rules.thirdPartyLicenses).containsExactlyElementsIn(rule.licenses);
+      }
+      if (rule.owners != null) {
+        assertThat(rules.thirdPartyOwners).containsExactlyElementsIn(rule.owners);
+      }
+    }
+  }
+
+  @Test
+  public void testBuildEachNamedRuleForbidden() {
+    for (String ruleName : CopyrightPatterns.lookup.keySet()) {
+      CopyrightPatterns.RuleSet.Builder builder =
+          CopyrightPatterns.RuleSet.builder().addForbidden(ruleName);
+
+      CopyrightPatterns.RuleSet rules = builder.build();
+
+      CopyrightPatterns.Rule rule = CopyrightPatterns.lookup.get(ruleName);
+
+      if (rule.exclusions != null) {
+        assertThat(rules.excludePatterns).containsExactlyElementsIn(rule.exclusions);
+      }
+      if (rule.licenses != null) {
+        assertThat(rules.forbiddenLicenses).containsExactlyElementsIn(rule.licenses);
+      }
+      if (rule.owners != null) {
+        assertThat(rules.forbiddenOwners).containsExactlyElementsIn(rule.owners);
+      }
+    }
+  }
+}
---
+++ src/test/java/com/googlesource/gerrit/plugins/copyright/tools/ScanToolTest.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# Copyright (C) 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+readonly scanner="${TEST_SRCDIR}/google3/java/com/google/devtools/compliance/copyright/scan_tool"
+readonly testdata="${TEST_SRCDIR}/google3/javatests/com/google/devtools/compliance/copyright/testdata"
+
+echo "Testing APACHE2 (unknown) license"
+output=$("${scanner}" "${testdata}/licenses/APACHE2.txt") || die "Failed scanning APACHE2 license."
+licenses=$(echo "${output}" | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "UNKNOWN" ]]; then
+  die "Expected unknown and only unknown licenses in APACHE2 license but found ${output}"
+fi
+
+echo "Testing ANDROID (3p) owner"
+output=$("${scanner}" "${testdata}/licenses/ANDROID.txt") || die "Failed scanning ANDROID owner."
+licenses=$(echo "${output}" | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party owners in ANDROID owner but found ${output}"
+fi
+
+echo "Testing first_party.zip deep"
+output=$("${scanner}" --deep "${testdata}/archives/first_party.zip") \
+    || die "Failed deep scanning first_party.zip"
+licenses=$(echo "${output}" | fgrep -v OWNER | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "UNKNOWN" ]]; then
+  die "Expected unknown and only unknown licenses in first_party.zip but found ${output}"
+fi
+licenses=$(echo "${output}" | fgrep OWNER | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party owners in first_party.zip but found ${output}"
+fi
+
+echo "Testing BSD2 (unknown) license"
+output=$("${scanner}" "${testdata}/licenses/BSD2.txt") || die "Failed scanning BSD2 license."
+licenses=$(echo "${output}" | fgrep -v OWNER | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "UNKNOWN" ]]; then
+  die "Expected unknown and only unknown licenses in BSD2 but found ${output}"
+fi
+licenses=$(echo "${output}" | fgrep OWNER | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party owners in BSD2 but found ${output}"
+fi
+
+echo "Testing MIT (unknown) license"
+output=$("${scanner}" "${testdata}/licenses/MIT.txt") || die "Failed scanning MIT license."
+licenses=$(echo "${output}" | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "UNKNOWN" ]]; then
+  die "Expected unknown and only unknown licenses in MIT license but found ${output}"
+fi
+
+echo "Testing UNKNOWN (3p) owner"
+output=$("${scanner}" "${testdata}/licenses/UNKNOWN.txt") || die "Failed scanning UNKNOWN owner."
+licenses=$(echo "${output}" | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party owners in UNKNOWN owner but found ${output}"
+fi
+
+echo "Testing third_party.tgz deep"
+output=$("${scanner}" --deep "${testdata}/archives/third_party.tgz") \
+    || die "Failed deep scanning third_party.tgz"
+licenses=$(echo "${output}" | fgrep -v OWNER | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "UNKNOWN" ]]; then
+  die "Expected unknown and only unknown licenses in third_party.zip but found ${output}"
+fi
+licenses=$(echo "${output}" | fgrep OWNER | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party owners in third_party.zip but found ${output}"
+fi
+
+echo "Testing AFFERO (unknown) license"
+output=$("${scanner}" "${testdata}/licenses/AFFERO.txt.gz") || die "Failed scanning AFFERO license."
+licenses=$(echo "${output}" | fgrep -v 'OWNER' | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "UNKNOWN" ]]; then
+  die "Expected unknown and only unknown licenses in AFFERO license but found ${output}"
+fi
+licenses=$(echo "${output}" | fgrep OWNER | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party owners in AFFERO license but found ${output}"
+fi
+
+echo "Testing forbidden.cpio deep"
+output=$("${scanner}" --deep "${testdata}/archives/forbidden.cpio") \
+    || die "Failed deep scanning forbidden.cpio"
+licenses=$(echo "${output}" | fgrep -v 'OWNER' | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "UNKNOWN" ]]; then
+  die "Expected unknown and only unknown licenses in forbidden.cpio but found ${output}"
+fi
+licenses=$(echo "${output}" | fgrep OWNER | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party owners in fobidden.cpio but found ${output}"
+fi
+
+echo "Testing scan of non-file error"
+"${scanner}" "${TEST_SRCDIR}/google3/javatests/com/google/devtools/compliance" 2>/dev/null \
+    && die "Expected directory to fail scan."
+
+echo "Testing scan of no files error"
+"${scanner}" 2>/dev/null && die "Expected scan of no files to fail."
+
+exit 0
