550b761b3ffe71a3dcc0f78975636165ca5739a7
Bob Badour <bbadour@google.com>
2019-03-26 20:07:36 -0700

    Initial release of copyright scanner library
    This copyright scanner library has been developed for the purpose of
    creating a plugin to ensure necessary review when new revisions may
    introduce copyrighted files that are not compliant with a project's
    policies around copyright.
    
    Initially targeted at the AOSP gerrit instances with the intent to
    expand to other Google-owned instances, the plugin will be completely
    configurable.
    
    Best practices identify allowed copyrights as first party or third
    party, and restrict third party copyrights to specific locations.
    
    See for example: https://opensource.google.com/docs/thirdparty/
    
    Revisions containing only first-party code do not require special
    review.
    
    Revisions to files in locations where third-party code is allowed that
    consist entirely of first-party code or appropriately licensed
    third-party code do not require special review.
    
    Revisions to files outside locations where third party licenses are
    allowed that appear to have third party licenses will require special
    review to verify they are false positives, or a qualified reviewer may
    reject the commit.
    
    Changes to files that seem to introduce unknown or forbidden licenses
    likewise require special review. A qualified reviewer may determine
    that an unknown license has acceptable terms and allow it. The reviewer
    may determine the match is a false positive or reject the change.
    
    See for example: https://opensource.google.com/docs/thirdparty/licenses/
    and: https://opensource.google.com/docs/using/agpl-policy/
    
    This commit does not include the plugin. It releases the copyright
    scanner library. The library has been extensively tested and used
    internally for analyzing content on AOSP hosts.
    
    The library and associated command-line tool supports deep scans into
    archive files (.zip, .jar, .apk etc.); however, the plugin will perform
    only shallow scans.
    
    Change-Id: I8800cf011f392d7d0c848f43a8efa095dd68ad0a

---
+++ src/main/java/com/googlesource/gerrit/plugins/copyright/tools/AndroidScan.java
@@ -0,0 +1,57 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.copyright.tools;
+
+import com.googlesource.gerrit.plugins.copyright.lib.CopyrightPatterns;
+import java.io.IOException;
+
+/** Runs the scan tool with patterns reflecting Android Open Source Project (AOSP) policies. */
+public class AndroidScan {
+
+  public static void main(String[] args) throws IOException {
+    ScanTool.toolName = "android_scan";
+    ScanTool.rules =
+        CopyrightPatterns.RuleSet.builder()
+            .exclude("EXAMPLES")
+            // 1p
+            .addFirstParty("APACHE2")
+            .addFirstParty("ANDROID")
+            .addFirstParty("GOOGLE")
+            .addFirstParty("EXAMPLES")
+            // 3p
+            .addThirdParty("BSD")
+            .addThirdParty("MIT")
+            .addThirdParty("EPL")
+            .addThirdParty("GPL2")
+            .addThirdParty("GPL3")
+            .addThirdParty("PSFL")
+            // Forbidden
+            .addForbidden("AGPL")
+            .addForbiddenLicense(".*(?:Previously|formerly) licen[cs]ed under.*")
+            .addForbidden("NOT_A_CONTRIBUTION")
+            .addForbidden("WTFPL")
+            .addForbidden("BEER_WARE")
+            .addForbidden("CC_BY_NC")
+            .addForbidden("NON_COMMERCIAL")
+            .addForbidden("COMMONS_CLAUSE")
+            .addForbidden("WATCOM")
+            .addForbidden("CC_BY_C")
+            .addForbidden("LGPL")
+            .addForbidden("GPL")
+            .build();
+
+    ScanTool.main(args);
+  }
+}
---
+++ src/test/java/com/googlesource/gerrit/plugins/copyright/tools/AndroidScanTest.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Copyright (C) 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+readonly scanner="${TEST_SRCDIR}/copyright/android_scan"
+readonly testdata="${TEST_SRCDIR}/copyright/src/test/java/com/googlesource/gerrit/plugins/copyright/testdata"
+
+function die() {
+  echo -e "$@" >&2
+  exit 1
+}
+
+echo "Testing APACHE2 (1p) license"
+output=$(echo "${testdata}/licenses/APACHE2.txt" | "${scanner}" --f=- -v) \
+    || die "Failed scanning APACHE2 license."
+licenses=$(echo "${output}" | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "FIRST_PARTY" ]]; then
+  die "Expected first party and only first party licenses in APACHE2 license but found ${output}"
+fi
+
+echo "Testing ANDROID (1p) owner"
+output=$("${scanner}" -v "${testdata}/licenses/ANDROID.txt") || die "Failed scanning ANDROID owner."
+licenses=$(echo "${output}" | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "FIRST_PARTY" ]]; then
+  die "Expected first party and only first party owners in ANDROID owner but found ${output}"
+fi
+
+echo "Testing first_party.zip deep"
+output=$("${scanner}" --deep "${testdata}/archives/first_party.zip") \
+    || die "Failed deep scanning first_party.zip"
+licenses=$(echo "${output}" | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "FIRST_PARTY" ]]; then
+  die "Expected first party and only first party owners in first_party.zip but found ${output}"
+fi
+
+echo "Testing BSD2 (3p) license"
+output=$("${scanner}" "${testdata}/licenses/BSD2.txt") || die "Failed scanning BSD2 license."
+licenses=$(echo "${output}" | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party licenses in BSD2 license but found ${output}"
+fi
+
+echo "Testing MIT (3p) license"
+output=$("${scanner}" "${testdata}/licenses/MIT.txt") || die "Failed scanning MIT license."
+licenses=$(echo "${output}" | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party licenses in MIT license but found ${output}"
+fi
+
+echo "Testing UNKNOWN (3p) owner"
+output=$("${scanner}" "${testdata}/licenses/UNKNOWN.txt") || die "Failed scanning UNKNOWN owner."
+licenses=$(echo "${output}" | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party owners in UNKNOWN owner but found ${output}"
+fi
+
+echo "Testing third_party.tgz deep"
+output=$("${scanner}" --deep "${testdata}/archives/third_party.tgz") \
+    || die "Failed deep scanning third_party.tgz"
+licenses=$(echo "${output}" | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party licenses in third_party.tgz but found ${output}"
+fi
+
+echo "Testing AFFERO (forbidden) license"
+output=$("${scanner}" "${testdata}/licenses/AFFERO.txt.gz") || die "Failed scanning AFFERO license."
+licenses=$(echo "${output}" | fgrep -v 'OWNER' | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "FORBIDDEN" ]]; then
+  die "Expected forbidden and only forbidden licenses in AFFERO license but found ${output}"
+fi
+licenses=$(echo "${output}" | fgrep OWNER | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party owners in AFFERO license but found ${output}"
+fi
+
+echo "Testing forbidden.cpio deep"
+output=$("${scanner}" --deep "${testdata}/archives/forbidden.cpio") \
+    || die "Failed deep scanning forbidden.cpio"
+licenses=$(echo "${output}" | fgrep -v 'OWNER' | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "FORBIDDEN" ]]; then
+  die "Expected forbidden and only forbidden licenses in forbidden.cpio (deep) but found ${output}"
+fi
+licenses=$(echo "${output}" | fgrep OWNER | cut -d ' ' -f1 | sort -u)
+if [[ "${licenses}" != "THIRD_PARTY" ]]; then
+  die "Expected third party and only third party owners in fobidden.cpio but found ${output}"
+fi
+
+echo "Testing scan of non-file error"
+"${scanner}" -v "${TEST_SRCDIR}/google3/javatests/com/google/devtools/compliance" 2>/dev/null \
+    && die "Expected directory to fail scan."
+
+echo "Testing scan of no files error"
+"${scanner}" 2>/dev/null && die "Expected scan of no files to fail."
+
+exit 0
