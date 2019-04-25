c29a78063cbd17a413c37cb3423d339209397859
Bob Badour <bbadour@google.com>
2019-04-24 17:10:25 -0700

    CopyrightConfig to manage plugin configurations.
    Copyright plugin part 2 of 3: CopyrightConfig manages and validates
    plugin configurations described in the gerrit.config fike, and in the
    All-Projects project.config and groups files.
    
    ScannerConfig wraps all of the parameters needed for scanning files.
    
    CheckConfig wraps facilitates validation of the configuration. It also
    provides the main entry point for a command-line tool to check for
    performance-harming regular expression patterns.
    
    When a new configuration changes the scanner pattern, the gerrit server
    may be configured to require the submitter run the command-line tool
    and copy its output to the commit message. By default, the server will
    require this process, which can be turned off by making the timeTestMax
    value 0.
    
    Change-Id: I82c034be01b9b33b1f57041627d6065e27b021db

--- src/test/java/com/googlesource/gerrit/plugins/copyright/CopyrightReviewApiTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/copyright/CopyrightReviewApiTest.java
@@ -18,15 +18,14 @@ import static com.google.common.truth.Truth.assertThat;
 import static com.googlesource.gerrit.plugins.copyright.lib.CopyrightScanner.MatchType.AUTHOR_OWNER;
 import static com.googlesource.gerrit.plugins.copyright.lib.CopyrightScanner.MatchType.LICENSE;
 import static com.googlesource.gerrit.plugins.copyright.lib.CopyrightScanner.PartyType.FIRST_PARTY;
-import static com.googlesource.gerrit.plugins.copyright.lib.CopyrightScanner.PartyType.THIRD_PARTY;
 import static com.googlesource.gerrit.plugins.copyright.lib.CopyrightScanner.PartyType.FORBIDDEN;
+import static com.googlesource.gerrit.plugins.copyright.lib.CopyrightScanner.PartyType.THIRD_PARTY;
 import static com.googlesource.gerrit.plugins.copyright.lib.CopyrightScanner.PartyType.UNKNOWN;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.truth.Correspondence;
@@ -57,16 +56,17 @@ public class CopyrightReviewApiTest {
 
   @Before
   public void setUp() throws Exception {
-    reviewApi = new CopyrightReviewApi(
-        null,
-        () -> pluginUser,
-        () -> currentUser,
-        identifiedUserFactory,
-        null,
-        null,
-        null,
-        null,
-        null);
+    reviewApi =
+        new CopyrightReviewApi(
+            null,
+            () -> pluginUser,
+            () -> currentUser,
+            identifiedUserFactory,
+            null,
+            null,
+            null,
+            null,
+            null);
   }
 
   @Test
@@ -97,9 +97,7 @@ public class CopyrightReviewApiTest {
   public void testAddReviewers_addCC() throws Exception {
     ReviewInput ri = new ReviewInput();
     ri = reviewApi.addReviewers(ri, ImmutableList.of("someone"), ReviewerState.CC);
-    assertThat(ri.reviewers)
-        .comparingElementsUsing(addressedTo())
-        .containsExactly("CC:someone");
+    assertThat(ri.reviewers).comparingElementsUsing(addressedTo()).containsExactly("CC:someone");
   }
 
   @Test
@@ -114,8 +112,9 @@ public class CopyrightReviewApiTest {
   @Test
   public void testAddReviewers_addMultiple() throws Exception {
     ReviewInput ri = new ReviewInput();
-    ri = reviewApi.addReviewers(
-        ri, ImmutableList.of("someone", "someone else"), ReviewerState.REVIEWER);
+    ri =
+        reviewApi.addReviewers(
+            ri, ImmutableList.of("someone", "someone else"), ReviewerState.REVIEWER);
     ri = reviewApi.addReviewers(ri, ImmutableList.of("another", "and another"), ReviewerState.CC);
     assertThat(ri.reviewers)
         .comparingElementsUsing(addressedTo())
@@ -164,10 +163,8 @@ public class CopyrightReviewApiTest {
 
   @Test
   public void testContainsComment_multipleDoContain() throws Exception {
-    ImmutableList<CommentInput> comments = ImmutableList.of(
-        CI("one", 1, 2),
-        CI("two", 806, 808),
-        CI("three", 3, 14));
+    ImmutableList<CommentInput> comments =
+        ImmutableList.of(CI("one", 1, 2), CI("two", 806, 808), CI("three", 3, 14));
     assertThat(reviewApi.containsComment(comments, CI("three", 3, 14))).isTrue();
     assertThat(reviewApi.containsComment(comments, CI("two", 806, 808))).isTrue();
     assertThat(reviewApi.containsComment(comments, CI("one", 1, 2))).isTrue();
@@ -175,183 +172,157 @@ public class CopyrightReviewApiTest {
 
   @Test
   public void testContainsComment_multipleDoNotContain() throws Exception {
-    ImmutableList<CommentInput> comments = ImmutableList.of(
-        CI("one", 1, 2),
-        CI("two", 806, 808),
-        CI("three", 3, 14));
+    ImmutableList<CommentInput> comments =
+        ImmutableList.of(CI("one", 1, 2), CI("two", 806, 808), CI("three", 3, 14));
     assertThat(reviewApi.containsComment(comments, CI("four", 806, 808))).isFalse();
   }
 
   @Test
   public void testReviewComments_firstParty() throws Exception {
     assertThat(
-        reviewApi.reviewComments(
-            "project",
-            FIRST_PARTY, // 1p license with 3p author is 1p license
-            false,
-            ImmutableList.of(lic1p(2), auth1p(3), lic1p(4), lic1p(120), auth3p(121), auth1p(122))
-        ))
-            .comparingElementsUsing(startsWithAndRangesMatch())
-            .containsExactly(
-                CI("First-party license :", 2, 4),
-                CI("First-party license :", 120, 120),
-                CI("Third-party author or owner :", 121, 121),
-                CI("First-party author or owner :", 122, 122));
+            reviewApi.reviewComments(
+                "project",
+                FIRST_PARTY, // 1p license with 3p author is 1p license
+                false,
+                ImmutableList.of(
+                    lic1p(2), auth1p(3), lic1p(4), lic1p(120), auth3p(121), auth1p(122))))
+        .comparingElementsUsing(startsWithAndRangesMatch())
+        .containsExactly(
+            CI("First-party license :", 2, 4),
+            CI("First-party license :", 120, 120),
+            CI("Third-party author or owner :", 121, 121),
+            CI("First-party author or owner :", 122, 122));
   }
 
   @Test
   public void testReviewComments_thirdPartyAllowed() throws Exception {
     assertThat(
-        reviewApi.reviewComments(
-            "project",
-            THIRD_PARTY, // 3p license and 1p license or author is 3p
-            true,
-            ImmutableList.of(lic3p(2), auth3p(3), lic3p(10), auth3p(200), auth3p(210), lic1p(211))
-        ))
-            .comparingElementsUsing(startsWithAndRangesMatch())
-            .containsExactly(
-                CI("Third-party license allowed", 2, 10),
-                CI("Third-party author or owner allowed", 200, 210),
-                CI("First-party license :", 211, 211));
+            reviewApi.reviewComments(
+                "project",
+                THIRD_PARTY, // 3p license and 1p license or author is 3p
+                true,
+                ImmutableList.of(
+                    lic3p(2), auth3p(3), lic3p(10), auth3p(200), auth3p(210), lic1p(211))))
+        .comparingElementsUsing(startsWithAndRangesMatch())
+        .containsExactly(
+            CI("Third-party license allowed", 2, 10),
+            CI("Third-party author or owner allowed", 200, 210),
+            CI("First-party license :", 211, 211));
   }
 
   @Test
   public void testReviewComments_thirdPartyNotAllowed() throws Exception {
     assertThat(
-        reviewApi.reviewComments(
-            "project",
-            THIRD_PARTY, // 3p license and 1p license or author is 3p
-            false,
-            ImmutableList.of(lic3p(2), auth3p(3), lic3p(10), auth3p(200), auth3p(210), auth1p(211))
-        ))
-            .comparingElementsUsing(startsWithAndRangesMatch())
-            .containsExactly(
-                CI("Third-party license disallowed", 2, 10),
-                CI("Third-party author or owner disallowed", 200, 210),
-                CI("First-party author or owner :", 211, 211));
+            reviewApi.reviewComments(
+                "project",
+                THIRD_PARTY, // 3p license and 1p license or author is 3p
+                false,
+                ImmutableList.of(
+                    lic3p(2), auth3p(3), lic3p(10), auth3p(200), auth3p(210), auth1p(211))))
+        .comparingElementsUsing(startsWithAndRangesMatch())
+        .containsExactly(
+            CI("Third-party license disallowed", 2, 10),
+            CI("Third-party author or owner disallowed", 200, 210),
+            CI("First-party author or owner :", 211, 211));
   }
 
   @Test
   public void testReviewComments_forbiddenAuthor() throws Exception {
     assertThat(
-        reviewApi.reviewComments(
-            "project",
-            FORBIDDEN, // forbidden author and anything else is still forbidden
-            false,
-            ImmutableList.of(lic1p(2), auth1p(3), lic1p(4), lic1p(120), authForbidden(121))
-        ))
-            .comparingElementsUsing(startsWithAndRangesMatch())
-            .containsExactly(
-                CI("First-party license :", 2, 4),
-                CI("First-party license :", 120, 120),
-                CI("Disapproved author or owner :", 121, 121));
+            reviewApi.reviewComments(
+                "project",
+                FORBIDDEN, // forbidden author and anything else is still forbidden
+                false,
+                ImmutableList.of(lic1p(2), auth1p(3), lic1p(4), lic1p(120), authForbidden(121))))
+        .comparingElementsUsing(startsWithAndRangesMatch())
+        .containsExactly(
+            CI("First-party license :", 2, 4),
+            CI("First-party license :", 120, 120),
+            CI("Disapproved author or owner :", 121, 121));
   }
 
   @Test
   public void testReviewComments_forbiddenLicense() throws Exception {
     assertThat(
-        reviewApi.reviewComments(
-            "project",
-            FORBIDDEN, // forbidden license and anything else is still forbidden
-            false,
-            ImmutableList.of(lic1p(2), auth1p(3), lic1p(4), lic1p(120), licForbidden(121))
-        ))
-            .comparingElementsUsing(startsWithAndRangesMatch())
-            .containsExactly(
-                CI("First-party license :", 2, 4),
-                CI("First-party license :", 120, 120),
-                CI("Disapproved license :", 121, 121));
+            reviewApi.reviewComments(
+                "project",
+                FORBIDDEN, // forbidden license and anything else is still forbidden
+                false,
+                ImmutableList.of(lic1p(2), auth1p(3), lic1p(4), lic1p(120), licForbidden(121))))
+        .comparingElementsUsing(startsWithAndRangesMatch())
+        .containsExactly(
+            CI("First-party license :", 2, 4),
+            CI("First-party license :", 120, 120),
+            CI("Disapproved license :", 121, 121));
   }
 
   @Test
   public void testReviewComments_unknownLicense() throws Exception {
     assertThat(
-        reviewApi.reviewComments(
-            "project",
-            FORBIDDEN, // an unknown license could be forbidden so always requires review
-            false,
-            ImmutableList.of(lic1p(2), auth1p(3), lic1p(4), lic1p(120), licUnknown(121))
-        ))
-            .comparingElementsUsing(startsWithAndRangesMatch())
-            .containsExactly(
-                CI("First-party license :", 2, 4),
-                CI("First-party license :", 120, 120),
-                CI("Unrecognized license :", 121, 121));
+            reviewApi.reviewComments(
+                "project",
+                FORBIDDEN, // an unknown license could be forbidden so always requires review
+                false,
+                ImmutableList.of(lic1p(2), auth1p(3), lic1p(4), lic1p(120), licUnknown(121))))
+        .comparingElementsUsing(startsWithAndRangesMatch())
+        .containsExactly(
+            CI("First-party license :", 2, 4),
+            CI("First-party license :", 120, 120),
+            CI("Unrecognized license :", 121, 121));
   }
 
   @Test
   public void testPartyType_firstPartyLicense() throws Exception {
     // 1p license with 3p author is 1p in open-source
-    assertThat(
-        reviewApi.partyType(ImmutableList.of(lic1p(1), auth3p(2), lic1p(3)))
-    ).isEqualTo(FIRST_PARTY);
+    assertThat(reviewApi.partyType(ImmutableList.of(lic1p(1), auth3p(2), lic1p(3))))
+        .isEqualTo(FIRST_PARTY);
   }
 
   @Test
   public void testPartyType_firstPartyOwner() throws Exception {
-    assertThat(
-        reviewApi.partyType(ImmutableList.of(auth1p(1), auth1p(2)))
-    ).isEqualTo(FIRST_PARTY);
+    assertThat(reviewApi.partyType(ImmutableList.of(auth1p(1), auth1p(2)))).isEqualTo(FIRST_PARTY);
   }
 
   @Test
   public void testPartyType_thirdPartyLicense() throws Exception {
     // 3p license with 1p license or author is 3p
-    assertThat(
-        reviewApi.partyType(ImmutableList.of(lic3p(1), lic1p(3), auth1p(4)))
-    ).isEqualTo(THIRD_PARTY);
+    assertThat(reviewApi.partyType(ImmutableList.of(lic3p(1), lic1p(3), auth1p(4))))
+        .isEqualTo(THIRD_PARTY);
   }
 
   @Test
   public void testPartyType_thirdPartyOwner() throws Exception {
     // 3p author and 1p author without any license is 3p
-    assertThat(
-        reviewApi.partyType(ImmutableList.of(auth3p(1), auth1p(2)))
-    ).isEqualTo(THIRD_PARTY);
+    assertThat(reviewApi.partyType(ImmutableList.of(auth3p(1), auth1p(2)))).isEqualTo(THIRD_PARTY);
   }
 
   @Test
   public void testPartyType_forbiddenLicense() throws Exception {
     // forbidden anything with anything else in any combination is forbidden
     assertThat(
-        reviewApi.partyType(
-            ImmutableList.of(
-                licForbidden(1),
-                licUnknown(2),
-                lic3p(3),
-                auth3p(4),
-                lic1p(5),
-                auth1p(6)))
-    ).isEqualTo(FORBIDDEN);
+            reviewApi.partyType(
+                ImmutableList.of(
+                    licForbidden(1), licUnknown(2), lic3p(3), auth3p(4), lic1p(5), auth1p(6))))
+        .isEqualTo(FORBIDDEN);
   }
 
   @Test
   public void testPartyType_forbiddenOwner() throws Exception {
     // forbidden anything with anything else in any combination is forbidden
     assertThat(
-        reviewApi.partyType(
-            ImmutableList.of(
-                authForbidden(1),
-                licUnknown(2),
-                lic3p(3),
-                auth3p(4),
-                lic1p(5),
-                auth1p(6)))
-    ).isEqualTo(FORBIDDEN);
+            reviewApi.partyType(
+                ImmutableList.of(
+                    authForbidden(1), licUnknown(2), lic3p(3), auth3p(4), lic1p(5), auth1p(6))))
+        .isEqualTo(FORBIDDEN);
   }
 
   @Test
   public void testPartyType_unknownLicense() throws Exception {
     // unknown license with anything but forbidden is unknown (possibly forbidden)
     assertThat(
-        reviewApi.partyType(
-            ImmutableList.of(
-                licUnknown(2),
-                lic3p(3),
-                auth3p(4),
-                lic1p(5),
-                auth1p(6)))
-    ).isEqualTo(UNKNOWN);
+            reviewApi.partyType(
+                ImmutableList.of(licUnknown(2), lic3p(3), auth3p(4), lic1p(5), auth1p(6))))
+        .isEqualTo(UNKNOWN);
   }
 
   private static class FakePluginUser extends PluginUser {
@@ -365,6 +336,7 @@ public class CopyrightReviewApiTest {
     public Object getCacheKey() {
       return "31415966";
     }
+
     @Override
     public GroupMembership getEffectiveGroups() {
       return null;
@@ -373,32 +345,32 @@ public class CopyrightReviewApiTest {
 
   private static Correspondence<CommentInput, CommentInput> startsWithAndRangesMatch() {
     return new Correspondence<CommentInput, CommentInput>() {
-        @Override
-        public boolean compare(CommentInput actual, CommentInput expected) {
-          return actual.range.startLine == expected.range.startLine
-              && actual.range.endLine == expected.range.endLine
-              && actual.message.startsWith(expected.message);
-        }
+      @Override
+      public boolean compare(CommentInput actual, CommentInput expected) {
+        return actual.range.startLine == expected.range.startLine
+            && actual.range.endLine == expected.range.endLine
+            && actual.message.startsWith(expected.message);
+      }
 
-        @Override
-        public String toString() {
-          return "starts with and ranges match";
-        }
-      };
+      @Override
+      public String toString() {
+        return "starts with and ranges match";
+      }
+    };
   }
 
   private static Correspondence<AddReviewerInput, String> addressedTo() {
     return new Correspondence<AddReviewerInput, String>() {
-        @Override
-        public boolean compare(AddReviewerInput actual, String expected) {
-          return expected.equals(actual.state().toString() + ":" + actual.reviewer);
-        }
+      @Override
+      public boolean compare(AddReviewerInput actual, String expected) {
+        return expected.equals(actual.state().toString() + ":" + actual.reviewer);
+      }
 
-        @Override
-        public String toString() {
-          return "addressed to";
-        }
-      };
+      @Override
+      public String toString() {
+        return "addressed to";
+      }
+    };
   }
 
   /** Comment input {@code text} from {@code start} line to {@code end} line. */
