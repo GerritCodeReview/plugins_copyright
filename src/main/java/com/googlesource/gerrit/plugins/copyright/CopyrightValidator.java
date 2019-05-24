89e7eb2933825594e70e3f8abe83f55e20a412b3
Bob Badour <bbadour@google.com>
2019-05-24 12:03:13 -0700

    Copyright plugin source.
    Touched all the source for the copyright plugin for gerrit team review.
    
    Omitted .../testdata/archives/ which contains binary archives
    containing files from .../testdata/licenses/
    
    Change-Id: Ifdc8aae584598ad4ce1f8c8372468b51fb593a4d

--- src/main/java/com/googlesource/gerrit/plugins/copyright/CopyrightValidator.java
+++ src/main/java/com/googlesource/gerrit/plugins/copyright/CopyrightValidator.java
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+//  http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,12 +26,6 @@ import com.google.gerrit.extensions.api.changes.ReviewResult;
 import com.google.gerrit.extensions.events.RevisionCreatedListener;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.extensions.restapi.RestApiException;
-import com.google.gerrit.metrics.Counter0;
-import com.google.gerrit.metrics.Counter1;
-import com.google.gerrit.metrics.Description;
-import com.google.gerrit.metrics.Description.Units;
-import com.google.gerrit.metrics.Field;
-import com.google.gerrit.metrics.MetricMaker;
 import com.google.gerrit.metrics.Timer0;
 import com.google.gerrit.metrics.Timer1;
 import com.google.gerrit.reviewdb.client.Project;
@@ -40,14 +34,12 @@ import com.google.gerrit.server.config.PluginConfig;
 import com.google.gerrit.server.config.PluginConfigFactory;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.inject.AbstractModule;
-import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.copyright.lib.CopyrightScanner.Match;
 import com.googlesource.gerrit.plugins.copyright.lib.IndexedLineReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
@@ -79,79 +71,6 @@ public class CopyrightValidator implements RevisionCreatedListener {
         DynamicSet.bind(binder(), RevisionCreatedListener.class).to(CopyrightValidator.class);
       }
     };
-  }
-
-  @Singleton
-  private static class Metrics {
-    final Counter0 scanCount;
-    final Timer0 scanRevisionTimer;
-    final Timer0 scanFileTimer;
-    final Counter1 scanCountByProject;
-    final Timer1 scanRevisionTimerByProject;
-    final Timer1 scanFileTimerByProject;
-    final Counter1 scanCountByBranch;
-    final Timer1 scanRevisionTimerByBranch;
-    final Timer1 scanFileTimerByBranch;
-
-    @Inject
-    Metrics(MetricMaker metricMaker) {
-      Field<String> project = Field.ofString("project", "project name");
-      Field<String> branch = Field.ofString("branch", "branch name");
-      scanCount =
-          metricMaker.newCounter(
-              "scan_count",
-              new Description("Total number of copyright scans").setRate().setUnit("scans"));
-      scanRevisionTimer =
-          metricMaker.newTimer(
-              "scan_revision_latency",
-              new Description("Time spent scanning entire revisions")
-                  .setCumulative()
-                  .setUnit(Units.MILLISECONDS));
-      scanFileTimer =
-          metricMaker.newTimer(
-              "scan_file_latency",
-              new Description("Time spent scanning each file")
-                  .setCumulative()
-                  .setUnit(Units.MICROSECONDS));
-      scanCountByProject =
-          metricMaker.newCounter(
-              "scan_count_by_project",
-              new Description("Total number of copyright scans").setRate().setUnit("scans"),
-              project);
-      scanRevisionTimerByProject =
-          metricMaker.newTimer(
-              "scan_revision_latency_by_project",
-              new Description("Time spent scanning entire revisions")
-                  .setCumulative()
-                  .setUnit(Units.MILLISECONDS),
-              project);
-      scanFileTimerByProject =
-          metricMaker.newTimer(
-              "scan_file_latency_by_project",
-              new Description("Time spent scanning each file")
-                  .setCumulative()
-                  .setUnit(Units.MICROSECONDS),
-              project);
-      scanCountByBranch =
-          metricMaker.newCounter(
-              "scan_count_by_branch",
-              new Description("Total number of copyright scans").setRate().setUnit("scans"),
-              branch);
-      scanRevisionTimerByBranch =
-          metricMaker.newTimer(
-              "scan_revision_latency_by_branch",
-              new Description("Time spent scanning entire revisions")
-                  .setCumulative()
-                  .setUnit(Units.MILLISECONDS),
-              branch);
-      scanFileTimerByBranch =
-          metricMaker.newTimer(
-              "scan_file_latency_by_branch",
-              new Description("Time spent scanning each file")
-                  .setCumulative()
-                  .setUnit(Units.MICROSECONDS),
-              branch);
-    }
   }
 
   @Inject
@@ -189,16 +108,18 @@ public class CopyrightValidator implements RevisionCreatedListener {
     if (scannerConfig == null) {
       // error already reported during configuration load
       logger.atWarning().log(
-          "%s plugin enabled with no configuration -- not scanning revision %s",
-          pluginName, event.getChange().currentRevision);
+          "plugin enabled with no configuration -- not scanning revision %s",
+          event.getChange().currentRevision);
+      metrics.skippedReviewWarnings.increment(project);
       return;
     }
     if (scannerConfig.scanner == null) {
       if (scannerConfig.hasErrors()) {
         // error already reported during configuration load
         logger.atWarning().log(
-            "%s plugin enabled with errors in configuration -- not scanning revision %s",
-            pluginName, event.getChange().currentRevision);
+            "plugin enabled with errors in configuration -- not scanning revision %s",
+            event.getChange().currentRevision);
+        metrics.skippedReviewWarnings.increment(project);
       } // else plugin not enabled
       return;
     }
@@ -210,7 +131,9 @@ public class CopyrightValidator implements RevisionCreatedListener {
       scanRevision(project, branch, event);
     } catch (IOException | RestApiException e) {
       logger.atSevere().withCause(e).log(
-          "%s plugin cannot scan revision %s", pluginName, event.getChange().currentRevision);
+          "cannot scan revision %s", event.getChange().currentRevision);
+      metrics.scanErrors.increment(project);
+      metrics.errors.increment();
       return;
     }
   }
@@ -228,12 +151,13 @@ public class CopyrightValidator implements RevisionCreatedListener {
       throws IOException, RestApiException {
     Map<String, ImmutableList<Match>> findings = new HashMap<>();
     ArrayList<String> containedPaths = new ArrayList<>();
-    long scanStart = System.nanoTime();
-    metrics.scanCount.increment();
     metrics.scanCountByProject.increment(project);
     metrics.scanCountByBranch.increment(branch);
 
-    try (Repository repo = repoManager.openRepository(Project.nameKey(project));
+    try (Timer0.Context t0 = metrics.scanRevisionTimer.start();
+        Timer1.Context t1project = metrics.scanRevisionTimerByProject.start(project);
+        Timer1.Context t1branch = metrics.scanRevisionTimerByBranch.start(branch);
+        Repository repo = repoManager.openRepository(Project.nameKey(project));
         RevWalk revWalk = new RevWalk(repo);
         TreeWalk tw = new TreeWalk(revWalk.getObjectReader())) {
       RevCommit commit = repo.parseCommit(ObjectId.fromString(event.getRevision().commit.commit));
@@ -259,8 +183,10 @@ public class CopyrightValidator implements RevisionCreatedListener {
             && !FileMode.REGULAR_FILE.equals(tw.getFileMode())) {
           continue;
         }
-        long fileStart = System.nanoTime();
-        try (ObjectReader reader = tw.getObjectReader();
+        try (Timer0.Context tf0 = metrics.scanFileTimer.start();
+            Timer1.Context tf1project = metrics.scanFileTimerByProject.start(project);
+            Timer1.Context tf1branch = metrics.scanFileTimerByBranch.start(branch);
+            ObjectReader reader = tw.getObjectReader();
             ObjectStream stream = reader.open(tw.getObjectId(0)).openStream()) {
           IndexedLineReader lineReader = new IndexedLineReader(fullPath, -1, stream);
           ImmutableList<Match> matches =
@@ -268,31 +194,26 @@ public class CopyrightValidator implements RevisionCreatedListener {
           if (!matches.isEmpty()) {
             findings.put(tw.getPathString(), matches);
           }
-          long fileTime = (System.nanoTime() - fileStart) / 1000;
-          metrics.scanFileTimer.record(fileTime, TimeUnit.MICROSECONDS);
-          metrics.scanFileTimerByProject.record(project, fileTime, TimeUnit.MICROSECONDS);
-          metrics.scanFileTimerByBranch.record(branch, fileTime, TimeUnit.MICROSECONDS);
         }
       }
     }
-    long scanTime = (System.nanoTime() - scanStart) / 1000000;
-    metrics.scanRevisionTimer.record(scanTime, TimeUnit.MILLISECONDS);
-    metrics.scanRevisionTimerByProject.record(project, scanTime, TimeUnit.MILLISECONDS);
-    metrics.scanRevisionTimerByBranch.record(branch, scanTime, TimeUnit.MILLISECONDS);
 
     ReviewResult result = reviewApi.reportScanFindings(project, scannerConfig, event, findings);
     if (result != null && result.error != null && !result.error.equals("")) {
       logger.atSevere().log(
-          "%s plugin revision %s: error posting review: %s",
-          pluginName, event.getChange().currentRevision, result.error);
+          "revision %s: error posting review: %s", event.getChange().currentRevision, result.error);
+      metrics.postReviewErrors.increment(project);
+      metrics.errors.increment();
     }
     if (result != null && result.reviewers != null) {
       for (Map.Entry<String, AddReviewerResult> entry : result.reviewers.entrySet()) {
         AddReviewerResult arr = entry.getValue();
         if (arr.error != null && !arr.error.equals("")) {
           logger.atSevere().log(
-              "%s plugin revision %s: error adding reviewer %s: %s",
-              pluginName, event.getChange().currentRevision, entry.getKey(), arr.error);
+              "revision %s: error adding reviewer %s: %s",
+              event.getChange().currentRevision, entry.getKey(), arr.error);
+          metrics.addReviewerErrors.increment(project);
+          metrics.errors.increment();
         }
       }
     }
--- tools/check_new_config.sh
+++ tools/check_new_config.sh
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#  http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -34,7 +34,7 @@ function usage() {
       echo
     else
       echo "The project.config full path must end with project.config -- not $(basename $2)"
-      ehco
+      echo
     fi
   fi
 }
